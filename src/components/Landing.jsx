be170cfdc23141653e781d7065cbd6074c1601d5
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-28 22:50:05 -0700

    add text overlay on landing img, apply font family, fix positioning

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,6 +7,15 @@ export default function Landing(){
         <div className="body">
             <a href="https://en.wikipedia.org/wiki/Mount_Rinjani" target="_blank">
             <img src={rinja} className="rinja" />
+            
+           
+                <div className="img-blurb">
+                    <h1 className="img-blurb libre-baskerville-regular lg-img-blurb">
+                        UNEARTH UNPARALLELED LANDSCAPES OF LOMBOK IN ITS IDYLLIC AND NATURAL SERENITY
+                    </h1>
+                
+                    <h2 className="libre-baskerville-regular-italic sm-img-blurb">Your private villa awaits. </h2> 
+                </div>
             </a>
         </div>
     )
