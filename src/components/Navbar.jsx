bf48b11eb371379e760cc8819cffeb031e7b37f5
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-31 23:38:16 -0700

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <div>
+            <a className="nav-item" href="">Accomodation</a>
+            <a className="nav-item" href="">About</a>
+            <a className="nav-item" href="">Contact</a>
+        </div>
+    )
+}
